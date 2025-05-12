4436e4329c72baff0218fdd32d111bdcdb3bf7bd
Toghrul Mammadli <98665062+TogrulMemmedli@users.noreply.github.com>
2025-05-12 18:28:05 +0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const movieRouter = require('./movie.routes');
 const genreRouter = require('./genre.routes');
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Movies
+ *     description: Endpoints for managing movies
+ *   - name: Genres
+ *     description: Endpoints for managing genres
+ */
 
 router.use('/movies', movieRouter);
 router.use('/genres', genreRouter);
